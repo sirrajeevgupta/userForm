5a072132c8bde49fd095b823cfff7df62318400c
sirrajeevgupta <rajeevsofficials@gmail.com>
2025-02-01 00:07:47 +0530

    fixing backend url

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
   //baseURL: 'http://localhost:3500',
-  baseURL: 'https://userform-api.onrender.com',
+  baseURL: 'https://userformapi.onrender.com',
 });
